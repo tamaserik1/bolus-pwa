b7819ce057702e6c57850368b1cb34d359e4959d
tamaserik1 <tamaserik1@gmail.com>
2026-02-08 22:36:13 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,20 @@
-const CACHE_NAME = "bolus-pwa-v1";
-const FILES = [
-"index.html",
-"manifest.json"
+const CACHE_NAME = 'bolus-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", e => {
-e.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e => {
-e.respondWith(
-caches.match(e.request).then(r => r || fetch(e.request))
-);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
