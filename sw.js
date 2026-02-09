98e84ff3fa00064e043f382cf1824c5ead08c0d7
tamaserik1 <tamaserik1@gmail.com>
2026-02-09 21:06:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,51 @@
-const CACHE_NAME = 'bolus-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_VERSION = 'v2';
+const CACHE_NAME = `bolus-cache-${CACHE_VERSION}`;
+
+const CORE_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(keys =>
+        Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      )
+    ])
   );
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  // iOS PWA navigation fix
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('/index.html').then(res => res || fetch('/index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return res;
+      });
+    })
   );
 });
