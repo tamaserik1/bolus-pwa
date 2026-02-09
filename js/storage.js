4208f5816480b2931d112946cb4177dbcc02223e
tamaserik1 <tamaserik1@gmail.com>
2026-02-09 21:44:35 +0100

    Delete js directory

--- js/storage.js
+++
@@ -1,8 +0,0 @@
-function getHistory(){
-  return JSON.parse(localStorage.getItem("boluses")||"[]");
-}
-function saveBolus(b){
-  const h=getHistory();
-  h.push(b);
-  localStorage.setItem("boluses",JSON.stringify(h));
-}
